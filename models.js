90b7de18b1a18aa8ef5486d086b57e0a41846b69
Justin Nietzer <justinnietzer@gmail.com>
2018-07-24 11:23:52 -0400

    added fixes

--- models.js
+++ models.js
@@ -1,43 +1,33 @@
-"use strict";
+'use strict';
 
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
 
-// this is our schema to represent a restaurant
 const blogPostSchema = mongoose.Schema({
-  title: { type: String, required: true },
-  content: { type: String, required: true },
-  author: { 
+  author: {
     firstName: String,
     lastName: String
-   },
-   created: {type: Date, default: Date.now}
+  },
+  title: {type: String, required: true},
+  content: {type: String},
+  created: {type: Date, default: Date.now}
 });
 
-// *virtuals* (http://mongoosejs.com/docs/guide.html#virtuals)
-// allow us to define properties on our object that manipulate
-// properties that are stored in the database. Here we use it
-// to generate a human readable string based on the address object
-// we're storing in Mongo.
-blogPostSchema.virtual("authorName").get(function() {
+
+blogPostSchema.virtual('authorName').get(function() {
   return `${this.author.firstName} ${this.author.lastName}`.trim();
 });
 
-// this is an *instance method* which will be available on all instances
-// of the model. This method will be used to return an object that only
-// exposes *some* of the fields we want from the underlying data
 blogPostSchema.methods.serialize = function() {
   return {
     id: this._id,
-    title: this.title,
-    content: this.content,
     author: this.authorName,
+    content: this.content,
+    title: this.title,
     created: this.created
-
   };
 };
 
-// note that all instance methods and virtual properties on our
-// schema must be defined *before* we make the call to `.model`.
-const BlogPost = mongoose.model("BlogPost", blogPostSchema);
+const BlogPost = mongoose.model('Post', blogPostSchema);
 
-module.exports = { BlogPost };
+module.exports = {BlogPost};
--- server.js
+++ server.js
@@ -1,18 +1,16 @@
-"use strict";
+'use strict';
 
-const express = require("express");
-const mongoose = require("mongoose");
-
-// Mongoose internally uses a promise-like object,
-// but its better to make Mongoose use built in es6 promises
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-// config.js is where we control constants for entire
-// app like PORT and DATABASE_URL
-const { PORT, DATABASE_URL } = require("./config");
-const { BlogPost } = require("./models");
+const { DATABASE_URL, PORT } = require('./config');
+const { BlogPost } = require('./models');
 
 const app = express();
+
+app.use(morgan('common'));
 app.use(express.json());
 
 app.get('/posts', (req, res) => {
@@ -27,21 +25,18 @@ app.get('/posts', (req, res) => {
     });
 });
 
-// can also request by ID
-app.get("/posts/:id", (req, res) => {
+app.get('/posts/:id', (req, res) => {
   BlogPost
-    // this is a convenience method Mongoose provides for searching
-    // by the object _id property
     .findById(req.params.id)
     .then(post => res.json(post.serialize()))
     .catch(err => {
       console.error(err);
-      res.status(500).json({ message: "Internal server error" });
+      res.status(500).json({ error: 'something went horribly awry' });
     });
 });
 
-app.post("/posts", (req, res) => {
-  const requiredFields = ["title", "content", "author", "created"];
+app.post('/posts', (req, res) => {
+  const requiredFields = ['title', 'content', 'author'];
   for (let i = 0; i < requiredFields.length; i++) {
     const field = requiredFields[i];
     if (!(field in req.body)) {
@@ -51,56 +46,68 @@ app.post("/posts", (req, res) => {
     }
   }
 
-  BlogPost.create({
-    title: req.body.title,
-    content: req.body.content,
-    author: req.body.author
-  })
+  BlogPost
+    .create({
+      title: req.body.title,
+      content: req.body.content,
+      author: req.body.author
+    })
     .then(blogPost => res.status(201).json(blogPost.serialize()))
     .catch(err => {
       console.error(err);
-      res.status(500).json({ message: "Internal server error" });
+      res.status(500).json({ error: 'Something went wrong' });
+    });
+
+});
+
+
+app.delete('/posts/:id', (req, res) => {
+  BlogPost
+    .findByIdAndRemove(req.params.id)
+    .then(() => {
+      res.status(204).json({ message: 'success' });
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: 'something went terribly wrong' });
     });
 });
 
-app.put("/posts/:id", (req, res) => {
-  // ensure that the id in the request path and the one in request body match
+
+app.put('/posts/:id', (req, res) => {
   if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
-    const message =
-      `Request path id (${req.params.id}) and request body id ` +
-      `(${req.body.id}) must match`;
-    console.error(message);
-    return res.status(400).json({ message: message });
+    res.status(400).json({
+      error: 'Request path id and request body id values must match'
+    });
   }
 
-  // we only support a subset of fields being updateable.
-  // if the user sent over any of the updatableFields, we udpate those values
-  // in document
-  const toUpdate = {};
-  const updateableFields = ["title", "content", "author"];
-
+  const updated = {};
+  const updateableFields = ['title', 'content', 'author'];
   updateableFields.forEach(field => {
     if (field in req.body) {
-      toUpdate[field] = req.body[field];
+      updated[field] = req.body[field];
     }
   });
 
   BlogPost
-    // all key/value pairs in toUpdate will be updated -- that's what `$set` does
-    .findByIdAndUpdate(req.params.id, { $set: toUpdate })
-    .then(post => res.status(204).end())
-    .catch(err => res.status(500).json({ message: "Internal server error" }));
+    .findByIdAndUpdate(req.params.id, { $set: updated }, { new: true })
+    .then(updatedPost => res.status(204).end())
+    .catch(err => res.status(500).json({ message: 'Something went wrong' }));
 });
 
-app.delete("/restaurants/:id", (req, res) => {
-  BlogPost.findByIdAndRemove(req.params.id)
-    .then(post => res.status(204).end())
-    .catch(err => res.status(500).json({ message: "Internal server error" }));
+
+app.delete('/:id', (req, res) => {
+  BlogPost
+    .findByIdAndRemove(req.params.id)
+    .then(() => {
+      console.log(`Deleted blog post with id \`${req.params.id}\``);
+      res.status(204).end();
+    });
 });
 
-// catch-all endpoint if client makes request to non-existent endpoint
-app.use("*", function(req, res) {
-  res.status(404).json({ message: "Not Found" });
+
+app.use('*', function (req, res) {
+  res.status(404).json({ message: 'Not Found' });
 });
 
 // closeServer needs access to a server object, but that only
@@ -111,23 +118,19 @@ let server;
 // this function connects to our database, then starts the server
 function runServer(databaseUrl, port = PORT) {
   return new Promise((resolve, reject) => {
-    mongoose.connect(
-      databaseUrl,
-      err => {
-        if (err) {
-          return reject(err);
-        }
-        server = app
-          .listen(port, () => {
-            console.log(`Your app is listening on port ${port}`);
-            resolve();
-          })
-          .on("error", err => {
-            mongoose.disconnect();
-            reject(err);
-          });
+    mongoose.connect(databaseUrl, err => {
+      if (err) {
+        return reject(err);
       }
-    );
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+        .on('error', err => {
+          mongoose.disconnect();
+          reject(err);
+        });
+    });
   });
 }
 
@@ -136,7 +139,7 @@ function runServer(databaseUrl, port = PORT) {
 function closeServer() {
   return mongoose.disconnect().then(() => {
     return new Promise((resolve, reject) => {
-      console.log("Closing server");
+      console.log('Closing server');
       server.close(err => {
         if (err) {
           return reject(err);
@@ -153,4 +156,4 @@ if (require.main === module) {
   runServer(DATABASE_URL).catch(err => console.error(err));
 }
 
-module.exports = { app, runServer, closeServer };
+module.exports = { runServer, app, closeServer };
